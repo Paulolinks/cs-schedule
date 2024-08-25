1003e4c51706bc19b5380c1e6cb1de2bed364322
paulolinks <paulolinks16@gmail.com>
2024-08-25 00:19:04 -0400

    ultimo  erro corrigido com sucesso

--- next.config.mjs
+++ next.config.mjs
@@ -1,21 +1,11 @@
-// @ts-check
-
-/**
- * @type {import('next').NextConfig}
- */
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-  // webpack5: true,
-  // webpack: (config) => {
-  //   config.resolve.fallback = { fs: false };
-  //   return config;
-  // },
-  /* config options here */
   typescript: {
     ignoreBuildErrors: true,
   },
   eslint: {
-    ignoreDuringBuilds: true, // Add this line
+    ignoreDuringBuilds: true,
   },
 };
 
-export default nextConfig;
+module.exports = nextConfig;
