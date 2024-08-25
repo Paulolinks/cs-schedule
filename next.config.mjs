3ae762b79b844e37436c4580fe8924f66c7ddb8c
paulolinks <paulolinks16@gmail.com>
2024-08-24 23:33:16 -0400

    3 terceiro  erro corrigido com sucesso

--- next.config.mjs
+++ next.config.mjs
@@ -4,11 +4,11 @@
  * @type {import('next').NextConfig}
  */
 const nextConfig = {
-  webpack5: true,
-  webpack: (config) => {
-    config.resolve.fallback = { fs: false };
-    return config;
-  },
+  // webpack5: true,
+  // webpack: (config) => {
+  //   config.resolve.fallback = { fs: false };
+  //   return config;
+  // },
   /* config options here */
   typescript: {
     ignoreBuildErrors: true,
