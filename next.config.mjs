528a9a611e66f17173dbb4c61c5abbb069c0c421
paulolinks <paulolinks16@gmail.com>
2024-08-25 00:24:25 -0400

    2 ultimo 2 erro corrigido com sucesso

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,8 @@
-/** @type {import('next').NextConfig} */
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
 const nextConfig = {
   typescript: {
     ignoreBuildErrors: true,
@@ -8,4 +12,4 @@ const nextConfig = {
   },
 };
 
-module.exports = nextConfig;
+export default nextConfig;
