d6399cf024c7ce10094a9c5b0ce17ee561f7cd96
paulolinks <paulolinks16@gmail.com>
2024-08-25 00:38:00 -0400

    2222 Fix __dirname issue in next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,9 +1,14 @@
-import path from "path";
+// next.config.mjs
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   webpack: (config) => {
-    config.resolve.alias["@"] = path.resolve("./src"); // Adjust path as needed
+    config.resolve.alias["@"] = path.resolve(__dirname, "src");
     return config;
   },
   typescript: {
