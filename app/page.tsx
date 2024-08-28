26105831380f81ec1d79bd427ca123b5cb9d382d
paulolinks <paulolinks16@gmail.com>
2024-08-27 22:15:11 -0400

    update 3.1 everything

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,3 @@
-/* eslint-disable no-unused-vars */
-
 import Image from "next/image";
 import Link from "next/link";
 
--- app/patients/[userId]/register/page.tsx
+++ app/patients/[userId]/register/page.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 import { redirect } from "next/navigation";
 
 import { getPatient, getUser } from "@/lib/actions/patient.actions";
-import RegisterForm from "@/components/forms/RegisterForm";
+import { RegisterForm } from "@/components/forms/RegisterForm";
 
 const Register = async ({ params: { userId } }: SearchParamProps) => {
   const user = await getUser(userId);
